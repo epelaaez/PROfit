a37468ea9d41734d0afc29abff488d2299aeb5eb
nathanielerowe <70993723+nathanielerowe@users.noreply.github.com>
2025-09-29 12:06:43 -0500

    attempt to fix merge conflict on branch

--- src/PROplot.cxx
+++ src/PROplot.cxx
@@ -434,6 +434,7 @@ getSplineGraphs(const PROsyst &systs, const PROconfig &config) {
                     if(best_fit) {
                         // int channel_start =  config.GetCollapsedGlobalVariableBinStart(global_channel_index, other_index);
                         size_t total_bins = config.GetChannelVariableBins(global_channel_index, other_index).NBins();
+                        Eigen::VectorXf this_bf_spec = config.GetChannelVariableBins(global_channel_index, other_index).ProjectSpectra(bf_spec(Eigen::seqN(channel_start, total_bins)), 0);
                         for(size_t bin = 0; bin < channel_nbins; bin++) {
                             bf_hist.SetBinContent(bin+1, bf_spec(bin+channel_start));
                         }
