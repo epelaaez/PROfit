0f1c7ec2be4fdfe5577589a86adabc2cf44250fd
Lee Hagaman <hagamanlee@gmail.com>
2026-01-26 15:48:38 -0500

    added knob count error message

--- src/PROsyst.cxx
+++ src/PROsyst.cxx
@@ -563,6 +563,12 @@ void PROsyst::FillSpline(const SystStruct& syst) {
     spline.bins = nbins;
     spline.segments_per_bin = knobvals.size(); 
 
+    if(syst.knobval.size() != syst.p_multi_spec.size()){
+        log<LOG_ERROR>(L"%1% || number of knobvals specified (%2%) does not match number of weight universes in file (%3%) for systematic %4%!") % __func__ % syst.knobval.size() % syst.p_multi_spec.size() % syst.systname.c_str();
+        log<LOG_ERROR>(L"Terminating.");
+        exit(EXIT_FAILURE);
+    }
+
     std::vector<SplineSegment> all_segments;
 
     for (size_t i = 0; i < nbins; ++i) {
